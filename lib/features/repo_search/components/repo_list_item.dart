9c5fa827285c273f0346572517ffb0c4c1f41dbb
Rerurate514 <rerurateyuto@gmail.com>
2025-04-12 18:44:11 +0900

    リポジトリ検索結果のListItemコンポーネントを追加

---
+++ lib/features/repo_search/components/repo_list_item.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:github_browser/core/components/info_chip.dart';
+import 'package:github_browser/features/repo_search/entities/repository.dart';
+
+class RepositoryListItem extends StatelessWidget {
+  final Repository repository;
+  
+  const RepositoryListItem({
+    super.key,
+    required this.repository,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(8.0),
+        onTap: () {
+          //TODO ページルート -> detals_page
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(repository.ownerIconUrl),
+                    radius: 20,
+                  ),
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: Text(
+                      repository.repositoryName,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 12),
+              Row(
+                children: [
+                  InfoChip(
+                    icon: Icons.code,
+                    label: repository.projectLanguage,
+                  ),
+                  const SizedBox(width: 8),
+                  InfoChip(
+                    icon: Icons.star,
+                    label: '${repository.starCount}',
+                  ),
+                  const SizedBox(width: 8),
+                  InfoChip(
+                    icon: Icons.call_split,
+                    label: '${repository.forkCount}',
+                  ),
+                  const SizedBox(width: 8),
+                  InfoChip(
+                    icon: Icons.error_outline,
+                    label: '${repository.issueCount}',
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
