94d7938e5fc51a74329337c3daf357d353660a40
Rerurate514 <rerurateyuto@gmail.com>
2025-04-13 07:54:15 +0900

    SettingPageへの遷移メソッドを作成

--- lib/core/routes/page_router.dart
+++ lib/core/routes/page_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:github_browser/features/repo_search/entities/repository.dart';
 import 'package:github_browser/pages/repo_detail_page.dart';
+import 'package:github_browser/pages/settings_page.dart';
 
 class PageRouter {
   static void navigateDetailsPage(BuildContext context, Repository repository){
@@ -12,9 +13,9 @@ class PageRouter {
   }
 
   static void navigateSettingsPage(BuildContext context){
-    // Navigator.push(
-    //   context,
-    //   MaterialPageRoute(builder: (context) => const SettingsPage()),
-    // );
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SettingsPage()),
+    );
   }
 }
