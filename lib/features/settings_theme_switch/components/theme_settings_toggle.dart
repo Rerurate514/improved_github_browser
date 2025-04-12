017981b5064f0a172f87528d758f6b4568a0a64e
Rerurate514 <rerurateyuto@gmail.com>
2025-04-12 22:44:49 +0900

    Themeを設定するトグルボタンコンポーネントの作成

---
+++ lib/features/settings_theme_switch/components/theme_settings_toggle.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:github_browser/core/components/toggle_button.dart';
+import 'package:github_browser/features/settings_theme_switch/providers/theme_mode_provider.dart';
+
+class ThemeSettingToggle extends ConsumerStatefulWidget {
+  const ThemeSettingToggle({super.key});
+
+  @override
+  ConsumerState<ThemeSettingToggle> createState() => _ThemeSettingToggleState();
+}
+
+class _ThemeSettingToggleState extends ConsumerState<ThemeSettingToggle> {
+  @override
+  Widget build(BuildContext context) {
+    final themeMode = ref.watch(themeModeProvider);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text("settings_toggle_darkMode"),
+        ToggleButton(
+          isActive: themeMode == ThemeMode.light ? false : true,
+          isToggledCallback: (bool isToggled) {
+            ref.read(themeModeProvider.notifier).setThemeMode(
+              isToggled
+              ? ThemeMode.dark 
+              : ThemeMode.light
+            );
+          },
+        )
+      ],
+    );
+  }
+}
