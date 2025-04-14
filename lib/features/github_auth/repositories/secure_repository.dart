82053c18da6ea78eb3f26bd8fab6f25f52e4e6f9
Rerurate514 <rerurateyuto@gmail.com>
2025-04-15 02:40:57 +0900

    secure_repositoryクラスを作成

---
+++ lib/features/github_auth/repositories/secure_repository.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+
+class GithubSecureRepository{
+  final FlutterSecureStorage _secureStorage;
+  static const String _tokenKey = 'github_auth_token';
+
+  GithubSecureRepository(): _secureStorage = const FlutterSecureStorage();
+
+
+  Future<void> saveToken(String token) async {
+    await _secureStorage.write(key: _tokenKey, value: token);
+  }
+
+  Future<String?> getToken() async {
+    return await _secureStorage.read(key: _tokenKey);
+  }
+
+  Future<void> deleteToken() async {
+    await _secureStorage.delete(key: _tokenKey);
+  }
+}
