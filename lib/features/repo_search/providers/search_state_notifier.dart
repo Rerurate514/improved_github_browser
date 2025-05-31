1a39f73495a2df4307c6f45be8c4fa5c62b7ad7d
Rerurate514 <rerurateyuto@gmail.com>
2025-06-01 04:10:18 +0900

    search_state_notifierにてネットワークエラーが起きた際の処理を追加

--- lib/features/repo_search/providers/search_state_notifier.dart
+++ lib/features/repo_search/providers/search_state_notifier.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:github_browser/core/utils/check_network_connection.dart';
 import 'package:github_browser/features/repo_search/entities/repository.dart';
 import 'package:github_browser/features/repo_search/providers/github_repository_provider.dart';
+import 'package:github_browser/l10n/app_localizations.dart';
 
 class SearchStateNotifier extends AutoDisposeNotifier<AsyncValue<List<Repository>>> {
   @override
@@ -10,6 +12,19 @@ class SearchStateNotifier extends AutoDisposeNotifier<AsyncValue<List<Repository
 
   Future<void> searchRepositories(String query) async {
     state = const AsyncValue.loading();
+
+    final bool isConnected = await checkNetworkConnection(
+      ref: ref,
+      isNotConnectedHandler: (context) {
+        state = AsyncValue.error(
+          context != null 
+            ? AppLocalizations.of(context).error_network
+            : 'Network error',
+          StackTrace.current
+        );
+      }
+    );
+    if(!isConnected) return;
 
     final repository = ref.read(githubRepositoryProvider);
 
