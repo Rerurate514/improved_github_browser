78a7d7332216122af090510d749c497278d53314
Rerurate514 <rerurateyuto@gmail.com>
2025-06-01 00:00:12 +0900

    theme_repositoryにてSharedPrefsをRiverpodでキャッシュ化

--- lib/features/settings_theme_switch/providers/theme_repository_provider.dart
+++ lib/features/settings_theme_switch/providers/theme_repository_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:github_browser/core/providers/shared_prefs_cache_provider.dart';
 import 'package:github_browser/features/settings_theme_switch/repositories/theme_repository.dart';
 
 final themeRepositoryProvider = Provider<ThemeRepository>((ref) {
-  return ThemeRepository();
+  return ThemeRepository(ref.read(sharedPrefsCacheProvider.notifier).state!);
 });
--- lib/features/settings_theme_switch/repositories/theme_repository.dart
+++ lib/features/settings_theme_switch/repositories/theme_repository.dart
@@ -3,14 +3,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeRepository {
   static const String _key = 'theme_mode';
+  final SharedPreferences prefs;
+  ThemeRepository(this.prefs);
   
   Future<void> saveThemeMode(ThemeMode mode) async {
-    final prefs = await SharedPreferences.getInstance();
     await prefs.setInt(_key, mode.index);
   }
   
   Future<ThemeMode> loadThemeMode() async {
-    final prefs = await SharedPreferences.getInstance();
     final value = prefs.getInt(_key);
     return value != null ? ThemeMode.values[value] : ThemeMode.light;
   }
