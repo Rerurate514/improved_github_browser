9d565a8ce1a481372b812369369d0563ddb57ced
Rerurate514 <rerurateyuto@gmail.com>
2025-06-02 18:49:58 +0900

    toggle_buttonをhook化

--- lib/core/components/toggle_button.dart
+++ lib/core/components/toggle_button.dart
@@ -1,32 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class ToggleButton extends StatefulWidget {
+class ToggleButton extends HookWidget {
   final void Function(bool) isToggledCallback;
   final bool isActive;
+
   const ToggleButton({super.key, required this.isToggledCallback, this.isActive = false});
 
   @override
-  State<ToggleButton> createState() => _ToggleButtonState();
-}
-
-class _ToggleButtonState extends State<ToggleButton> {
-  @override
   Widget build(BuildContext context) {
-    bool isToggled = widget.isActive;
-    
+    final isToggled = useState<bool>(isActive);
+
+    useEffect(() {
+      isToggled.value = isActive;
+      return null;
+    }, [isActive]);
+
     return Switch(
-      value: isToggled,
+      value: isToggled.value,
       activeColor: Colors.blue,
-      // ignore: deprecated_member_use
       activeTrackColor: Colors.blue.withOpacity(0.5),
       inactiveThumbColor: Colors.grey,
-      // ignore: deprecated_member_use
       inactiveTrackColor: Colors.grey.withOpacity(0.5),
       onChanged: (bool value) {
-        setState(() {
-          isToggled = value;
-          widget.isToggledCallback(isToggled);
-        });
+        isToggled.value = value;
+        isToggledCallback(value);
       },
     );
   }
